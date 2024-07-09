4010ea491f73b045cef338f74b047a852c23bbf2
Josh Smith <jnsmith1020@gmail.com>
2024-07-08 21:27:32 -0400

    Refactor date calculations

--- js/index.js
+++ js/index.js
@@ -1,42 +1,35 @@
+const utcToEst = (date) => {
+    date.setHours(date.getHours() + 4)
+    return date
+}
 
-const calculateTimeElapsed = (start, end) => {
+const calculateTimeElapsed = (start, end, format = "ym") => {
     const startYear = start.getFullYear()
-    const startMonth = start.getMonth() + 1
+    const startMonth = start.getMonth()
     const endYear = end.getFullYear()
-    const endMonth = end.getMonth() + 1
+    const endMonth = end.getMonth()
     const invalidRangeError = "End date cannot be older than start date."
     let yearsElapsed = endYear - startYear
-    let monthsElapsed = 0
+    let monthsElapsed = endMonth - startMonth
     let timeElapsed = ""
 
-    if (yearsElapsed < 0) {
+    if (yearsElapsed < 0 || monthsElapsed < 0 && yearsElapsed < 1) {
         console.error(invalidRangeError)
-    } 
-    
-    if (yearsElapsed > 0) {
-        if (endMonth < startMonth) {
-            yearsElapsed -= 1
-            monthsElapsed = 12 - startMonth + endMonth
-        } else {
-            monthsElapsed = endMonth - startMonth
-        }
+    }
 
+    if (monthsElapsed < 0) {
+        yearsElapsed -= 1
+        monthsElapsed += 12
+    }
+
+    if (yearsElapsed > 0 && format.includes("y")) {
         timeElapsed = `${yearsElapsed} year`
         timeElapsed += yearsElapsed == 1 ? "" : "s"
-        
-        if (monthsElapsed > 0) {
-            timeElapsed += ` ${monthsElapsed} month`
-            timeElapsed += monthsElapsed == 1 ? "" : "s"
-        }
-    } else if (yearsElapsed == 0) {
-        monthsElapsed = endMonth - startMonth
-        
-        if (monthsElapsed > 0) {
-            timeElapsed = `${monthsElapsed} month`
-            timeElapsed += monthsElapsed == 1 ? "" : "s"
-        } else {
-            console.error(invalidRangeError)
-        }
+    }
+    
+    if (monthsElapsed > 0 && format.includes("m")) {
+        timeElapsed += ` ${monthsElapsed} month`
+        timeElapsed += monthsElapsed == 1 ? "" : "s"
     }
 
     return timeElapsed
@@ -48,7 +41,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
         now = new Date(Date.now())
         startDate = date.dataset.start ? new Date(Date.parse(date.dataset.start)) : now
         endDate = date.dataset.end ? new Date(Date.parse(date.dataset.end)) : now
-        timeElapsed = calculateTimeElapsed(startDate, endDate)
+        timeElapsed = calculateTimeElapsed(
+            utcToEst(startDate),
+            utcToEst(endDate),
+            date.dataset.format,
+        )
 
         if (timeElapsed) {
             date.innerText = timeElapsed
