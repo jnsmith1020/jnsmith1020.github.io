ded5e2136e61317151f50595a3f49f415dfaff32
Josh Smith <jnsmith1020@gmail.com>
2024-07-13 20:45:03 -0400

    Add page outline

---
+++ js/page-outline.js
@@ -0,0 +1,55 @@
+class PageOutline {
+    isExpanded = false
+
+    constructor(element) {
+        this.element = element
+        this.children = element.querySelectorAll(".js-page-outline__list-item")
+        this.toggleButton = element.querySelector(".js-page-outline__toggle")
+        this.init()
+    }
+
+    init() {
+        this.initChildren()
+        this.initToggleButton()
+    }
+
+    initChildren() {
+        this.hide()
+    }
+
+    initToggleButton() {
+        this.toggleButton.onclick = () => {
+            this.toggle()
+        }
+    }
+
+    show() {
+        this.children.forEach((child, index) => {
+            if (index > 2)
+                child.classList.remove("sr-only")
+        })
+        this.toggleButton.innerText = "Show less"
+        this.isExpanded = true
+    }
+
+    hide() {
+        this.children.forEach((child, index) => {
+            if (index > 2)
+                child.classList.add("sr-only")
+        })
+        this.toggleButton.innerText = "Show more"
+        this.isExpanded = false
+    }
+
+    toggle() {
+        this.isExpanded ? this.hide() : this.show()
+    }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const elements = document.querySelectorAll(".js-page-outline")
+
+    elements.forEach((element) => {
+        const outline = new PageOutline(element)
+    })
+})
